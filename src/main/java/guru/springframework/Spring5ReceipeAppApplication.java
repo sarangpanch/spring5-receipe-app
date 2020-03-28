1a1dcdd47b68e4dc59d1acf95b3a4f0b8b8f78e9
Sarang <sarangdp@gmail.com>
2020-03-28 17:48:42 -0400

    New project.Initial version

--- src/main/java/guru/springframework/Spring5ReceipeAppApplication.java
+++ src/main/java/guru/springframework/Spring5ReceipeAppApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Spring5ReceipeAppApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(Spring5ReceipeAppApplication.class, args);
     }
