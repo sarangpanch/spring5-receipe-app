a59efd52b6c76176be14b55bcf56186ba6372d39
Sarang <sarangdp@gmail.com>
2020-04-03 08:53:10 -0400

    New project.Initial version

--- src/main/java/guru/springframework/Spring5RecipeAppApplication.java
+++ src/main/java/guru/springframework/Spring5RecipeAppApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Spring5ReceipeAppApplication {
+public class Spring5RecipeAppApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Spring5ReceipeAppApplication.class, args);
+        SpringApplication.run(Spring5RecipeAppApplication.class, args);
     }
 
 }
